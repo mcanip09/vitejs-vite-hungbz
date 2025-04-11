5c934c893c338711da4c8db8047e9bb11f8223a0
mcanip09 <mehmet_canip@hotmail.com>
2025-04-11 22:23:29 +0300

    kareler update

--- src/components/Kareler.jsx
+++ src/components/Kareler.jsx
@@ -1,20 +1,24 @@
-import React from 'react'; /* ADIM 0 */
+import React, { useState } from 'react'; /* ADIM 0 */
 
 // Bu değişkeni YALNIZCA bir state'in ilk değeri olarak kullanın!
 const KareIdListesi = ['sqA', 'sqB', 'sqC', 'sqD'];
 
 export default function Kareler() {
   // ADIM 1: State'leri burada tanımlayabilirsiniz.
+  const [kareler] = useState(KareIdListesi);
+  const [aktifKare,setAktifKare] = useState("")
 
   const classAdiAl = (id) => {
     // ADIM 2: Bu bir click handler değil, JSX içinde kullanılan bir yardımcı(helper) fonksiyondur.(aşağıda kullanımına bakabilirsin)
     // Eğer argüman olarak verilen id state'de tutulan id ile eşleşirse, class adı 'active' olan bir string döndürmeli
     // diğer durumlar için boş string dönecek.
+    return id === aktifKare ? 'active' : '';
   };
 
   const aktifEt = (id) => {
     // ADIM 3: Bu click handler, tıklandığında aktif kareyi tutan state'i setter'ı ile güncellemeli.
-    // eğer tıklanan önceden aktifse, aktifliğini kaldırmalıyız. (aktif kareyi tutan state'i sıfırlayabiliriz)
+    // eğer tıklanan önceden aktifse, aktifliğini kaldırmalıyız. (aktif kareyi tutan state'i sıfırlayabiliriz) 
+    aktifKare === id ? setAktifKare("") : setAktifKare(id);
   };
 
   return (
@@ -23,7 +27,7 @@ export default function Kareler() {
       <div className="squares">
         {
           // ADIM 4: 'KareIdListesi' değişkeni yerine bir state'den listeyi alıp .map() ile dönmek daha doğru olur.
-          KareIdListesi.map((id) => (
+          kareler.map((id) => (
             <div
               id={id}
               key={id}
